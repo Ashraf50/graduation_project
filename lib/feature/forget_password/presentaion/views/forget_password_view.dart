2424093baec67f2b8cbee51e4c79ddbaa91b4733
ibrahim alnagar <alnnagaribrahim45@gmail.com>
2024-11-01 09:57:46 +0200

    create forgetPassword feature and it's view and body

---
+++ lib/feature/forget_password/presentaion/views/forget_password_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_project/feature/forget_password/presentaion/views/widgets/forget_password_view_body.dart';
+
+import '../../../../core/widget/custom_scaffold.dart';
+
+class ForgetPasswordView extends StatelessWidget {
+  const ForgetPasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      body: ForgetPasswordViewBody(),
+    );
+  }
+}
