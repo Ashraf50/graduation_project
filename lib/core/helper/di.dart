6340e7aad76f1ce0a9d820a8efef9ff3be99a0e4
ibrahim alnagar <alnnagaribrahim45@gmail.com>
2025-06-20 21:43:36 +0300

    refactor: remove unused flat feature imports from dependency injection

--- lib/core/helper/di.dart
+++ lib/core/helper/di.dart
@@ -10,14 +10,7 @@ import 'package:graduation_project/feature/account/data/repository/repo/account_
 import 'package:graduation_project/feature/account/domain/repository/data_souce/account_data_source_contrcat.dart';
 import 'package:graduation_project/feature/account/domain/repository/repo/account_repo_contrcat.dart';
 import 'package:graduation_project/feature/account/domain/usecase/upload_image_usecase.dart';
-import 'package:graduation_project/feature/flat/data/repository/repo/flat_repo_impl.dart';
-import 'package:graduation_project/feature/flat/domain/repository/repo/flat_repo_contract.dart';
-import 'package:graduation_project/feature/flat/presentation/view_model/flat_view_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
-import '../../feature/flat/data/manager/flat_supabase_manager.dart';
-import '../../feature/flat/data/repository/data_source/flat_supabase_data_source_impl.dart';
-import '../../feature/flat/domain/use_case/add_flat_with_image_use_case.dart';
 
 final supabase = Supabase.instance.client;
 
@@ -48,4 +41,3 @@ AuthRemoteDataSourceContract injectAuthRemoteDataSourceContract() {
   return AuthSupabaseDataSourceImpl(
       supabaseManager: AuthSupabaseManager.getInstance());
 }
-
